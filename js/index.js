afae47bf7622e65549671ab99da6da5674087930
unknown <Joaqun@XPS13.valessentia.local>
2015-05-30 19:49:17 +0200

    Server recibe movimiento del jugador

---
+++ js/index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var io = require('socket.io')();
+var map = require('Map')();
+
+io.on('connection', function(socket) {
+  
+  var player = new Player(socket.id)
+  map.addPlayer(player)
+
+  socket.on('movePlayer', function(direction) {
+    map.playerMoveDirection(player, direction)
+    io.emit('updates', map.getUpdates())
+    map.cleanUpdates()
+  })
+
+  socket.on('disconnect', function() {
+    
+  })
+})
+
