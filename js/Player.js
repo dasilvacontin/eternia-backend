64c2cf52608b7c3e96f61b4d45f1b886f46506a0
David da Silva <dasilvacontin@gmail.com>
2015-05-30 21:17:35 +0200

    added some helpers to Player

--- js/Player.js
+++ js/Player.js
@@ -1,9 +1,11 @@
 'use strict'
 
+var util = require('./util')
+
 function Player (playerId, cellId) {
   this.id = playerId
   this.hp = this.maxHP = 100
-  this.cell = cellId
+  this.cellId = cellId
 }
 
 Player.prototype.isAlive = function () {
@@ -12,6 +14,22 @@ Player.prototype.isAlive = function () {
 
 Player.prototype.takeDamage = function (damage) {
   this.hp = Math.max(0, this.hp - damage)
+}
+
+Player.prototype.getPositionObject = function () {
+  return util.getPositionFromId(this.cellId)
+}
+
+Player.prototype.getId = function () {
+  return this.id
+}
+
+Player.prototype.setCellId = function (cellId) {
+  this.cellId = cellId
+}
+
+Player.prototype.getCellId = function () {
+  return this.cellId
 }
 
 module.exports = Player
